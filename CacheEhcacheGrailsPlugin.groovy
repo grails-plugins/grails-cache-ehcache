3a5e5a91f48318e96092aed40d3e2a0dd6429a5c
Burt Beckwith <beckwithb@vmware.com>
2012-07-04 19:33:43 -0400

    1.0.0 release

--- CacheEhcacheGrailsPlugin.groovy
+++ CacheEhcacheGrailsPlugin.groovy
@@ -26,7 +26,7 @@ class CacheEhcacheGrailsPlugin {
 
 	private final Logger log = LoggerFactory.getLogger('grails.plugin.cache.CacheEhcacheGrailsPlugin')
 
-	String version = '1.0.0.BUILD-SNAPSHOT'
+	String version = '1.0.0'
 	String grailsVersion = '2.0 > *'
 	def loadAfter = ['cache']
 	def pluginExcludes = [
