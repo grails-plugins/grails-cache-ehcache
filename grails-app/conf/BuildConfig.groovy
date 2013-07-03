7eb9c9652884fa175863ba1b9cb0b68d193d3d65
Burt Beckwith <burt@burtbeckwith.com>
2013-07-03 14:55:03 -0400

    dependency updates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,5 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for gh-pages branch
-grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
@@ -9,16 +8,20 @@ grails.project.dependency.resolution = {
 
 	repositories {
 		grailsCentral()
+		mavenLocal()
+		mavenCentral()
 	}
 
 	dependencies {
 		compile 'net.sf.ehcache:ehcache-core:2.4.6'
 
-        test 'org.codehaus.gpars:gpars:1.0.0'
+		test 'org.codehaus.gpars:gpars:1.0.0', {
+			export = false
+		}
 	}
 
 	plugins {
-		build(':release:2.0.4', ':rest-client-builder:1.0.2') {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
 		}
 		compile ':cache:1.1.1'
