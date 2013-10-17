3c6c31449810f63e246284b6fdebf7a724b55b86
Craig Andrews <candrews@integralblue.com>
2013-10-17 12:04:24 -0400

    Set the version to 1.0.1-SNAPSHOT

--- CacheEhcacheGrailsPlugin.groovy
+++ CacheEhcacheGrailsPlugin.groovy
@@ -26,7 +26,7 @@ class CacheEhcacheGrailsPlugin {
 
 	private final Logger log = LoggerFactory.getLogger('grails.plugin.cache.CacheEhcacheGrailsPlugin')
 
-	String version = '1.0.0'
+	String version = '1.0.1-SNAPSHOT'
 	String grailsVersion = '2.0 > *'
 	def loadAfter = ['cache']
 	def pluginExcludes = [
