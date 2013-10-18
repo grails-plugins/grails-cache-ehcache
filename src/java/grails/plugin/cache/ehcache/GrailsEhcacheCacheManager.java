grails-plugins/grails-cache-ehcache

commit 2271e7e6b608b69c854bd561877e122c85ed485c

Fixes #GPCACHEEHCACHE-5 (Jira Issue - Parallel access to getCache() can result in 'cache already exists' exception)

src/java/grails/plugin/cache/ehcache/GrailsEhcacheCacheManager.java
@@ -22,7 +22,11 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+import net.sf.ehcache.CacheException;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Ehcache;
 import net.sf.ehcache.Status;
@@ -40,6 +44,7 @@ import org.springframework.util.Assert;
  * @author Costin Leau
  * @author Juergen Hoeller
  * @author Burt Beckwith
+ * @author Andrew Walters
  */
 public class GrailsEhcacheCacheManager implements GrailsCacheManager, InitializingBean {
 
@@ -49,23 +54,37 @@ public class GrailsEhcacheCacheManager implements GrailsCacheManager, Initializi
 	protected final ConcurrentMap<String, Cache> cacheMap = new ConcurrentHashMap<String, Cache>();
 	protected Set<String> cacheNames = new LinkedHashSet<String>();
 
+    private final Lock lock = new ReentrantLock();
+
 	public Cache getCache(String name) {
-
-		Cache cache = cacheMap.get(name);
-		if (cache == null) {
-			// check the EhCache cache again (in case the cache was added at runtime)
-			Ehcache ehcache = cacheManager.getEhcache(name);
-			if (ehcache == null) {
-				// create a new one based on defaults
-				cacheManager.addCache(name);
-				ehcache = cacheManager.getEhcache(name);
-			}
-
-			cache = new GrailsEhcacheCache(ehcache);
-			addCache(cache);
+        Cache cache = cacheMap.get(name);
+        if (cache == null) {
+            try {
+                // Ensure we don't have parallel access to cache creation which can lead to 'cache already exists' exceptions
+                if (lock.tryLock(200, TimeUnit.MILLISECONDS)) {
+                    try {
+                        // check the EhCache cache again (in case the cache was added at runtime)
+                        Ehcache ehcache = cacheManager.getEhcache(name);
+                        if (ehcache == null) {
+                            // create a new one based on defaults
+                            cacheManager.addCache(name);
+                            ehcache = cacheManager.getEhcache(name);
+                            cache = new GrailsEhcacheCache(ehcache);
+                            addCache(cache);
+                        }
+                    } finally {
+                        lock.unlock();
+                    }
+                } else {
+                    throw new CacheException("Failed to get lock for " + name + " cache creation");
+                }
+            } catch(InterruptedException e) {
+                throw new CacheException("Failed to get lock for " + name + " cache creation");
+            }
 		}
-		return cache;
-	}
+
+        return cache;
+    }
 
 	public boolean cacheExists(String name) {
 		return getCacheNames().contains(name);

grails-app/conf/BuildConfig.groovy
@@ -13,6 +13,8 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 		compile 'net.sf.ehcache:ehcache-core:2.4.6'
+
+        test 'org.codehaus.gpars:gpars:1.0.0'
 	}
 
 	plugins {

test/unit/grails/plugin/cache/ehcache/EhcacheCacheManagerTests.groovy
@@ -1,0 +1,63 @@
+package grails.plugin.cache.ehcache
+
+import grails.test.GrailsUnitTestCase
+import grails.test.mixin.TestFor
+import groovyx.gpars.GParsPool
+import net.sf.ehcache.CacheManager
+import org.junit.Before
+
+/**
+ * @author Andrew Walters
+ */
+class EhcacheCacheManagerTests extends GrailsUnitTestCase {
+
+    protected GrailsEhcacheCacheManager manager
+
+    @Before
+    void before() {
+        manager = new GrailsEhcacheCacheManager()
+        manager.cacheManager = CacheManager.create()
+    }
+
+    void "test cache creation serial access"() {
+        (0..10).each {
+            manager.getCache("testCache")
+        }
+    }
+
+    /**
+     * As a parallel access isn't guaranteed to result in traversing the code at the same time, loop a number
+     * of times to try and cause the simultaneous access to getCache()
+     */
+    void "test cache creation parallel access"() {
+        assertFalse(manager.cacheExists("testCache"))
+
+        10.times {
+            println "Trying to get a failure x ${it + 1}"
+
+            GParsPool.withPool {
+                (0..4).everyParallel {
+                    manager.getCache("testCache") != null
+                }
+
+                manager.destroyCache("testCache")
+            }
+        }
+    }
+
+    void "test cache get parallel access"() {
+        manager.getCache("testCache")
+
+        assertTrue(manager.cacheExists("testCache"))
+
+        10.times {
+            println "Trying to get a failure x ${it + 1}"
+
+                GParsPool.withPool {
+                (0..10).eachParallel {
+                    assertNotNull(manager.getCache("testCache"))
+                }
+            }
+        }
+    }
+}

commit 5d3e9136428c3ca9d378eb1bb27ac1c9ee23f143

Regardless of if the ehCache exists or not, it must be registered with the Grails cache manager.
This fixes the integration tests which have been broken since 2271e7e6b608b69c854bd561877e122c85ed485c

src/java/grails/plugin/cache/ehcache/GrailsEhcacheCacheManager.java
@@ -68,10 +68,10 @@ public class GrailsEhcacheCacheManager implements GrailsCacheManager, Initializi
                         if (ehcache == null) {
                             // create a new one based on defaults
                             cacheManager.addCache(name);
-                            ehcache = cacheManager.getEhcache(name);
-                            cache = new GrailsEhcacheCache(ehcache);
-                            addCache(cache);
                         }
+                        ehcache = cacheManager.getEhcache(name);
+                        cache = new GrailsEhcacheCache(ehcache);
+                        addCache(cache);
                     } finally {
                         lock.unlock();
                     }