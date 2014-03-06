a30648822ced109d1237bfc15ecca04bcee019bf
Craig Andrews <candrews@integralblue.com>
2014-03-06 16:27:33 -0500

    Updating docs for Travis build: https://travis-ci.org/grails-plugins/grails-cache-ehcache/builds/20220372

--- doc.sh
+++ doc.sh
@@ -1,6 +1,22 @@
-rm -rf docs/manual
-grails doc --pdf --stacktrace | grep -v javadoc
-#grails doc --stacktrace | grep -v javadoc
-rm -rf docs/manual/api
-rm -rf docs/manual/gapi
-grails add-tracking
+TRAVIS_REPO_SLUG=grails-plugins/grails-cache-ehcache
+TRAVIS_BUILD_ID=20220372
+GIT_NAME=Craig Andrews
+GIT_EMAIL=candrews@integralblue.com
+GIT_TOKEN=b25c145b92657f247a737902ade2528e487f4ae9
+
+rm -rf docs
+grails doc --pdf
+
+git config user.name $GIT_NAME
+git config user.email $GIT_EMAIL
+git config credential.helper "store --file=.git/credentials"
+echo "https://$GH_TOKEN:@github.com" > .git/credentials
+git clone https://github.com:$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch
+cd gh-pages
+git rm -rf *
+cp -r ../docs .
+git add *
+git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+git push origin HEAD
+cd ..
+rm -rf gh-pages
