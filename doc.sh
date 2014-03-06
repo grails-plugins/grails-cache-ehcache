3833289659d5d37a404f6b1ccb18abd25eb8cb29
Craig Andrews <candrews@integralblue.com>
2014-03-06 18:19:31 -0500

    Include GH_TOKEN in the clone URL

--- doc.sh
+++ doc.sh
@@ -12,7 +12,7 @@ git config --global user.name "$GIT_NAME" &&
 git config --global user.email "$GIT_EMAIL" &&
 git config --global credential.helper "store --file=~/.git-credentials" &&
 echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials &&
-git clone https://github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch &&
+git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null &&
 cd gh-pages &&
 git rm -rf . &&
 cp -r ../docs . &&
