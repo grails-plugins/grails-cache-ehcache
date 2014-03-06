ab004c774bb17352e4b10d42806b55ef9de1a523
Craig Andrews <candrews@integralblue.com>
2014-03-06 18:07:34 -0500

    Since ~/.git/ doesn't necessarily exist, use ~/.git-credentials

--- doc.sh
+++ doc.sh
@@ -10,8 +10,8 @@ rm -rf docs &&
 ./grailsw doc --pdf &&
 git config --global user.name "$GIT_NAME" &&
 git config --global user.email "$GIT_EMAIL" &&
-git config --global credential.helper "store --file=~/.git/credentials" &&
-echo "https://$GH_TOKEN:@github.com" > ~/.git/credentials &&
+git config --global credential.helper "store --file=~/.git-credentials" &&
+echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials &&
 git clone https://github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch &&
 cd gh-pages &&
 git rm -rf . &&
