30868fe5eeaa379f60e9db543d9c8f4d588435e9
Craig Andrews <candrews@integralblue.com>
2014-03-06 17:53:11 -0500

    Globally set the git username and email

--- doc.sh
+++ doc.sh
@@ -8,8 +8,8 @@
 
 rm -rf docs &&
 ./grailsw doc --pdf &&
-git config user.name "$GIT_NAME" &&
-git config user.email "$GIT_EMAIL" &&
+git config --global user.name "$GIT_NAME" &&
+git config --global user.email "$GIT_EMAIL" &&
 git config credential.helper "store --file=.git/credentials" &&
 echo "https://$GH_TOKEN:@github.com" > .git/credentials &&
 git clone https://github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch &&
